e5c775977e56edeec6119cf02abd23d8f6067628
szc126 <>
2020-06-17 19:23:46 -0700

    update nogeoldae.sh

--- nogeoldae.sh
+++ nogeoldae.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python3 nogeoldae.py /tmp/sdl/no{1,2}/* -c | gedit - &
-python3 nogeoldae.py /tmp/sdl/no{1,2}/* -c -p | gedit - &
+python3 nogeoldae.py /tmp/sdl/no{1,2}/* -c text | gedit - &
+python3 nogeoldae.py /tmp/sdl/no{1,2}/* -c html > /tmp/N.htm; firefox /tmp/N.htm
