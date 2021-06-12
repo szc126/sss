047f41d7097c1a463675db9ec7ff450e4f4dbba1
szc126 <29127915+szc126@users.noreply.github.com>
2021-06-11 21:32:45 -0700

    add GitHub Actions

---
+++ jungno-dl.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/tmp/sdl.py "ko:重刊老乞大諺解 001.pdf" -o "/tmp/sdl/jungno-a"
+/tmp/sdl.py "ko:重刊老乞大諺解 002.pdf" -o "/tmp/sdl/jungno-b"
--- jungno.sh
+++ jungno.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-python3 jungno.py /tmp/sdl/no{1,2}/* -c text > /tmp/jungno.txt &
-python3 jungno.py /tmp/sdl/no{1,2}/* -c html > /tmp/jungno.htm; dragon /tmp/jungno.htm &
+
+./jungno.py /tmp/sdl/jungno-{a,b}/* -c text > "/tmp/jungno.txt" &
+./jungno.py /tmp/sdl/jungno-{a,b}/* -c html > "/tmp/jungno.htm"; dragon "/tmp/jungno.htm" &
